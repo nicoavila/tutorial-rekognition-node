cc20236d3088aef968f5545a52e81cd9b176361a
Nicolas Avila <nicolasavila.a@gmail.com>
2018-05-05 17:18:11 -0300

    feat: commit inicial

---
+++ app.js
@@ -0,0 +1,69 @@
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv').config();
+const fs = require('fs');
+
+//Creación y configuración de instancia de AWS Rekognition
+const rekognition = new AWS.Rekognition({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: '2016-06-27'
+});
+
+//Lee la imagen, la convierte a Buffer y prepara el objeto
+const bitmap = fs.readFileSync('./input/imagen1.jpg');
+let image = {
+  Image: {
+    Bytes: new Buffer(bitmap)
+  },
+  Attributes: ['ALL']
+}
+
+//Llama al método detectFaces
+rekognition.detectFaces(image, (error, data) => {
+  if (error) {
+    throw new Error(error);
+  }
+  data.FaceDetails.forEach((response) => {
+    let maxConfidence = response.Emotions.reduce((a, b) => b.Confidence > b.Confidence ? b : a);
+    let emotionName = '';
+    switch (maxConfidence.Type) {
+      case 'HAPPY':
+        emotionName = 'ALEGRÍA';
+      break;
+
+      case 'SAD':
+        emotionName = 'PENA';
+      break;
+      
+      case 'ANGRY':
+        emotionName = 'ENOJO';
+      break;
+      
+      case 'CONFUSED':
+        emotionName = 'CONFUSIÓN';
+      break;
+      
+      case 'DISGUSTED':
+        emotionName = 'DISGUSTO';
+      break;
+      
+      case 'SURPRISED':
+        emotionName = 'SORPRESA';
+      break;
+      
+      case 'CALM':
+        emotionName = 'CALMA';
+      break;
+
+      case 'UNKNOWN':
+        emotionName = 'DESCONOCIDA';
+      break;
+
+      default:
+        emotionName = 'DESCONOCIDA';
+      break;
+    }
+    console.log(`La emoción predominante en la imágen es ${emotionName} con un grado de seguridad de ${maxConfidence.Confidence}`);
+  });
+});
